9f0fd0171ce0337ad40e44558d03a49a7efe5cc7
techsubash <139789120+techsubash@users.noreply.github.com>
2023-07-18 19:25:52 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+public class Factorial{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 {
+			int i,fact=1;
+			int number=5;
+			for(i=1;i<=number;i++) {
+				fact=fact*i;
+			System.out.println("factorial is"+fact);
+			}
+
+	}
+
+	}}
